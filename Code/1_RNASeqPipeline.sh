d004cafce0215b4c7be2003e50d6c61a73fafbff
LiQun95 <liqun95@163.com>
2023-11-23 11:50:53 +0100

    update

--- Code/1_RNASeqPipeline.sh
+++ Code/1_RNASeqPipeline.sh
@@ -1,7 +1,13 @@
 ###
- # @Descripttion: 
+ # @Descripttion: RNASeq Pipeline
  # @Author: Qun Li
  # @Email: qun.li@ki.se
  # @Date: 2023-11-23 11:25:54
- # @LastEditTime: 2023-11-23 11:25:55
+ # @LastEditTime: 2023-11-23 11:50:43
 ### 
+
+### 1. QC and Trim
+#### software trim_galore
+#### https://www.bioinformatics.babraham.ac.uk/projects/trim_galore/
+
+trim_galore -q 30 -stringency 3 -length 20 --phred33 -fastqc -clip_R1 3 -clip_R2 3 -e 0.1 -dont_gzip -paired $SAMPLE_DIR/$R1_FASTQ $SAMPLE_DIR/$R2_FASTQ --output_dir $TRIM_DIR
