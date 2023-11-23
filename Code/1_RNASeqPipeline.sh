a0f4c3d792a0ddbcf3896d867a32f68f2a8e31df
LiQun95 <liqun95@163.com>
2023-11-23 13:58:02 +0100

    update

--- Code/1_RNASeqPipeline.sh
+++ Code/1_RNASeqPipeline.sh
@@ -3,16 +3,54 @@
  # @Author: Qun Li
  # @Email: qun.li@ki.se
  # @Date: 2023-11-23 11:25:54
- # @LastEditTime: 2023-11-23 13:25:15
+ # @LastEditTime: 2023-11-23 13:57:12
 ### 
 
+#--------------------------------------------------------------------------------
+### Default setting
+#### samples dir path
+SAMPLE_DIR=
+#### genome index path
+GENOMEINDEX=
+#### RSEM index path
+RSEMINDEX=
+#### gtf path
+ANNOTATION=$3
+#### FASTQ1
+FQ1=
+#### FASTQ2
+FQ2=
+
+#--------------------------------------------------------------------------------
+
+
+
+
+#--------------------------------------------------------------------------------
 ### 1. QC and Trim
-#### software trim_galore
+#### software: trim_galore
 #### https://www.bioinformatics.babraham.ac.uk/projects/trim_galore/
-#### $SAMPLE_DIR: samples' dir
-#### $TRIM_DIR: Results' dir after QC and trim 
-trim_galore -q 30 -stringency 3 -length 20 --phred33 -fastqc -clip_R1 3 -clip_R2 3 -e 0.1 -dont_gzip -paired $SAMPLE_DIR/$R1_FASTQ $SAMPLE_DIR/$R2_FASTQ --output_dir $TRIM_DIR
 
+trim_galore -q 30 -stringency 3 -length 20 --phred33 -fastqc \
+            -clip_R1 3 -clip_R2 3 -e 0.1 -dont_gzip \
+            -paired $SAMPLE_DIR/$FQ1 $SAMPLE_DIR/$FQ2 \
+            --output_dir $TRIM_DIR
+#--------------------------------------------------------------------------------
 
+#--------------------------------------------------------------------------------
 ### 2. Mapping
 #### Before start, you need get reference, annotation files ready
+#### software: STAR & RSEM
+#### https://github.com/alexdobin/STAR
+#### https://deweylab.github.io/RSEM/
+
+STAR --outSAMtype BAM SortedByCoordinate --runThreadN $THREADS \
+        --readFilesCommand zcat --genomeDir $GENOMEINDEX \
+        --readFilesIn $FQ1 $FQ2 --quantMode TranscriptomeSAM GeneCounts \
+        --outFileNamePrefix $STAROUT/${FQ1}_${FQ2}/${FQ1}_${FQ2}
+
+rsem-calculate-expression --paired-end -no-bam-output --alignments \
+        -p $THREADS ${FQ1}_${FQ2}Aligned.toTranscriptome.out.bam \
+        ${RSEMINDEX} $RSEMOUT/${FQ1}_${FQ2}
+#--------------------------------------------------------------------------------
+
