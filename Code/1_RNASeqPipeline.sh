56d6a135c40b1459eb3169f57e76e49413283cb7
LiQun95 <liqun95@163.com>
2023-11-23 13:25:27 +0100

    update

--- Code/1_RNASeqPipeline.sh
+++ Code/1_RNASeqPipeline.sh
@@ -3,7 +3,7 @@
  # @Author: Qun Li
  # @Email: qun.li@ki.se
  # @Date: 2023-11-23 11:25:54
- # @LastEditTime: 2023-11-23 11:54:55
+ # @LastEditTime: 2023-11-23 13:25:15
 ### 
 
 ### 1. QC and Trim
@@ -12,3 +12,7 @@
 #### $SAMPLE_DIR: samples' dir
 #### $TRIM_DIR: Results' dir after QC and trim 
 trim_galore -q 30 -stringency 3 -length 20 --phred33 -fastqc -clip_R1 3 -clip_R2 3 -e 0.1 -dont_gzip -paired $SAMPLE_DIR/$R1_FASTQ $SAMPLE_DIR/$R2_FASTQ --output_dir $TRIM_DIR
+
+
+### 2. Mapping
+#### Before start, you need get reference, annotation files ready
