c1bf9cdd83b99ce4a46d8ad9275b71c386bcf246
LiQun95 <liqun95@163.com>
2023-12-03 10:48:05 +0100

    update

--- Code/3_DEG.R
+++ Code/3_DEG.R
@@ -1,8 +1,22 @@
 ###
- # @Descripttion: Differential gene expression analysis
+ # @Descripttion: Differential gene expression analysis (DEG)
  # @Author: Qun Li
  # @Email: qun.li@ki.se
  # @Date: 2023-11-23 14:21:57
  # @LastEditTime: 2023-11-23 14:21:58
 ### 
+
+# DEG analysis was performed using R package DESeq2
+# plot figures using R package pheatmap
+
+# load libraries
+library(DESeq2)
+library(pheatmap)
+####
+# > packageVersion('DESeq2')
+# [1] ‘1.34.0’
+# > packageVersion('pheatmap')
+# [1] ‘1.0.12’
+####
+
 
