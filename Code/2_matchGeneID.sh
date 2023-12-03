739d4d4d7c844121514003a188605f6db3a78c55
LiQun95 <liqun95@163.com>
2023-12-03 10:40:14 +0100

    update

---
+++ Code/2_matchGeneID.sh
@@ -0,0 +1,11 @@
+###
+ # @Descripttion: Match gene ID
+ # @Author: Qun Li
+ # @Email: qun.li@ki.se
+ # @Date: 2023-12-03 10:32:46
+ # @LastEditTime: 2023-12-03 10:38:51
+### 
+
+# You can match Gene symbol and Ensembl ID using Ensembl BioMart Tools online
+# https://www.ensembl.org/biomart/martview
+# Also, we provided the matched ID table (matchedID.txt, 2023-12-03) in this repository
