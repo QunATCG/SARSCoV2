2b8c820a413fff1e2179f660ede592a0e1cf0fdf
LiQun95 <liqun95@163.com>
2023-12-06 20:35:27 +0100

    update

--- Code/4_DataAnalysis.R
+++ Code/4_DataAnalysis.R
@@ -40,6 +40,14 @@ hisat2_total <- read.table("./Data/ExpRNAseq/Hisat2/Hisat2_exp_total.txt", heade
 hisat2_covid <- read.table("./Data/ExpRNAseq/Hisat2/Hisat2_exp_Covid19.txt", header = T,
                            sep = "\t", stringsAsFactors = FALSE)
 
+Covid19_geneName <- c("ORF1ab","ORF1ab","S", "ORF3a", "E", "M", "ORF6", "ORF7a","ORF7b",
+                      "ORF8", "N", "ORF10")
+star_covid$Gene <- Covid19_geneName
+hisat2_covid$Gene <- Covid19_geneName
+
+# exclude duplicated genes
+
+
 # function definition
 qunDEGAnalysisTwoCondition <- function(dat, condition_CT, num_CT, condition_Treat, num_Treat, fc, pthreshold){
   coldata <- data.frame(condition = factor(c(rep(condition_CT, num_CT), rep(condition_Treat, num_Treat)), levels = c(condition_CT, condition_Treat)))
@@ -66,63 +74,104 @@ qunDEGAnalysisThreeCondition <- function(dat, condition_CT, num_CT, condition_Tr
   return(resultList)
 }
 
-keepOver1 <- function(dat, cutoff){
+keepOverExp <- function(dat, cutoff){
   res <- dat[which(rowMeans(dat[,c("Mock_1_FPKM", "Mock_2_FPKM", "Mock_3_FPKM")]) >=cutoff | 
                          rowMeans(dat[,c("T_1_FPKM", "T_2_FPKM", "T_3_FPKM")]) >=cutoff | 
                          rowMeans(dat[,c("NT_1_FPKM", "NT_2_FPKM", "NT_3_FPKM")]) >=cutoff),]
 }
 
-filterResult <- function(dat, expData, nameTag, fc, pthreshold){
-  res1 <- data.frame(results(dat, name=nameTag), stringsAsFactors = FALSE, check.names = FALSE)
-  res1$Ensembl <- rownames(res1)
-  res1[which(res1$log2FoldChange >= log2(fc) & res1$padj < pthreshold),'sig'] <- 'up'
-  res1[which(res1$log2FoldChange <= -log2(fc) & res1$padj < pthreshold),'sig'] <- 'down'
-  res1[which(abs(res1$log2FoldChange) <= log2(fc) | res1$padj >= pthreshold),'sig'] <- 'none'
-  res1_select <- subset(res1, sig %in% c('up', 'down'))
-  res <- merge(res1_select, expData, by = "Ensembl", all.x = T)
+keepOverReadCount <- function(dat, cutoff, tagItem){
+  if(tagItem == "star_total"){
+    res <- dat[which(rowSums(dat[,c("Mock_1_ReadCount", "Mock_2_ReadCount", "Mock_3_ReadCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("NT_1_ReadCount", "NT_2_ReadCount", "NT_3_ReadCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("T_1_ReadCount", "T_2_ReadCount", "T_3_ReadCount")] >= cutoff ) >= 3),]
+  }
+  if(tagItem == "star_covid19"){
+    res <- dat[which(rowSums(dat[,c("Mock_1_ExpectedCount", "Mock_2_ExpectedCount", "Mock_3_ExpectedCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("NT_1_ExpectedCount", "NT_2_ExpectedCount", "NT_3_ExpectedCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("T_1_ExpectedCount", "T_2_ExpectedCount", "T_3_ExpectedCount")] >= cutoff ) >= 3),]
+  }
+  if(tagItem == "hisat2_total"){
+    res <- dat[which(rowSums(dat[,c("Mock_1_ReadCount", "Mock_2_ReadCount", "Mock_3_ReadCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("NT_1_ReadCount", "NT_2_ReadCount", "NT_3_ReadCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("T_1_ReadCount", "T_2_ReadCount", "T_3_ReadCount")] >= cutoff ) >= 3),]
+  }
+  if(tagItem == "hisat2_covid19"){
+    res <- dat[which(rowSums(dat[,c("Mock_1_ReadCount", "Mock_2_ReadCount", "Mock_3_ReadCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("NT_1_ReadCount", "NT_2_ReadCount", "NT_3_ReadCount")] >= cutoff ) >= 3 |
+                       rowSums(dat[,c("T_1_ReadCount", "T_2_ReadCount", "T_3_ReadCount")] >= cutoff ) >= 3),]
+  }
   return(res)
 }
 
+filterResult <- function(dat, expData, nameTag, fc, pthreshold, tagItem){
+  if(tagItem == "three"){
+    res1 <- data.frame(results(dat, name=nameTag), stringsAsFactors = FALSE, check.names = FALSE)
+    res1$Ensembl <- rownames(res1)
+    res1[which(res1$log2FoldChange >= log2(fc) & res1$padj < pthreshold),'sig'] <- 'up'
+    res1[which(res1$log2FoldChange <= -log2(fc) & res1$padj < pthreshold),'sig'] <- 'down'
+    res1[which(abs(res1$log2FoldChange) <= log2(fc) | res1$padj >= pthreshold),'sig'] <- 'none'
+    res1_select <- subset(res1, sig %in% c('up', 'down'))
+    res <- merge(res1_select, expData, by = "Ensembl", all.x = T)
+    return(res)
+  }
+  if(tagItem == "two"){
+    res1 <- data.frame(dat, stringsAsFactors = FALSE, check.names = FALSE)
+    res1$Ensembl <- rownames(res1)
+    res <- merge(res1, expData, by = "Ensembl", all.x = T)
+    return(res)
+  }
+}
+
 # expression filter
-# exculde genes with fpkm <= 1 
-star_total_over1 <- keepOver1(star_total,1)
-star_covid_over1 <- keepOver1(star_covid,1)
+# exculde genes with fpkm <= 1 and readcounts <= 5
+star_total_over <- keepOverReadCount(keepOverExp(star_total,1),5,"star_total")
+star_covid_over <- keepOverReadCount(keepOverExp(star_covid,1),5,"star_covid19")
 
-hisat2_total_over1 <- keepOver1(hisat2_total,1)
-hisat2_covid_over1 <- keepOver1(hisat2_covid,1)
+hisat2_total_over <- keepOverReadCount(keepOverExp(hisat2_total,1),5,"hisat2_total")
+hisat2_covid_over <- keepOverReadCount(keepOverExp(hisat2_covid,1),5,"hisat2_covid19")
 
+star_total_over <- star_total
+star_covid_over <- star_covid
 
 # PCA
-star_total_over1_PCA <- star_total_over1[,30:38]
-rownames(star_total_over1_PCA) <- star_total_over1$Ensembl
-star_total_over1_PCA_DEseq2 <- qunDEGAnalysisThreeCondition(round(star_total_over1_PCA), "Mock", 3, "NT", 3, "T", 3, 2, 0.01)
-plotDispEsts(star_total_over1_PCA_DEseq2$DEG, main="Dispersion plot")
-rld_star_total_over1 <- rlog(star_total_over1_PCA_DEseq2$DEG)
-star_total_over1_PCA_dataFrame <- plotPCA(rld_star_total_over1, returnData = T)
+star_total_over_PCA <- star_total_over[,30:38]
+rownames(star_total_over_PCA) <- star_total_over$Ensembl
+star_total_over_PCA_DEseq2 <- qunDEGAnalysisThreeCondition(round(star_total_over_PCA), "Mock", 3, "NT", 3, "T", 3, 2, 0.01)
+plotDispEsts(star_total_over_PCA_DEseq2$DEG, main="Dispersion plot")
+rld_star_total_over <- rlog(star_total_over_PCA_DEseq2$DEG)
+star_total_over_PCA_dataFrame <- plotPCA(rld_star_total_over, returnData = T)
 
-pdf("./Results/0_PCA.pdf", width = 6.29, height = 2.26)
-ggplot(data = star_total_over1_PCA_dataFrame, mapping = aes(x = PC1, y = PC2, colour = group)) + geom_point(size = 1) +
+hisat2_total_over_PCA <- hisat2_total_over[,21:29]
+rownames(hisat2_total_over_PCA) <- hisat2_total_over$Ensembl
+hisat2_total_over_PCA_DEseq2 <- qunDEGAnalysisThreeCondition(round(hisat2_total_over_PCA), "Mock", 3, "NT", 3, "T", 3, 2, 0.01)
+plotDispEsts(hisat2_total_over_PCA_DEseq2$DEG, main="Dispersion plot")
+rld_hisat2_total_over <- rlog(hisat2_total_over_PCA_DEseq2$DEG)
+hisat2_total_over_PCA_dataFrame <- plotPCA(rld_hisat2_total_over, returnData = T)
+
+#pdf("./Results/0_PCA.pdf", width = 6.29, height = 2.26)
+ggplot(data = hisat2_total_over_PCA_dataFrame, mapping = aes(x = PC1, y = PC2, colour = group)) + geom_point(size = 1) +
   scale_color_manual(values = c( Mock = "red", NT = "blue", T = "green" )) +
   xlab("PC1: 90% variance") +
   ylab("PC2: 4% variance") +
   theme_bw() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
-dev.off()
+#dev.off()
 
-rld_star_total_over1_mat <- assay(rld_star_total_over1)
-rld_star_total_over1_cor <- cor(rld_star_total_over1_mat)
+rld_hisat2_total_over_mat <- assay(rld_hisat2_total_over)
+rld_hisat2_total_over_cor <- cor(rld_hisat2_total_over_mat)
 
-pdf("./Results/0_Pheatmap.pdf", width = 6.29, height = 5.68)
-pheatmap(rld_star_total_over1_cor)
-dev.off()
+#pdf("./Results/0_Pheatmap.pdf", width = 6.29, height = 5.68)
+pheatmap(rld_hisat2_total_over_cor)
+#dev.off()
 
 #vst_star_total_over1 <- vst(star_total_over1_PCA_DEseq2$DEG)
 #plotPCA(vst_star_total_over1)
 
-hisat2_total_over1_PCA <- hisat2_total_over1[,21:29]
-rownames(hisat2_total_over1_PCA) <- hisat2_total_over1$Ensembl
-hisat2_total_over1_PCA_DEseq2 <- qunDEGAnalysisThreeCondition(round(hisat2_total_over1_PCA), "Mock", 3, "NT", 3, "T", 3, 2, 0.01)
+#hisat2_total_over_PCA <- hisat2_total_over[,21:29]
+#rownames(hisat2_total_over_PCA) <- hisat2_total_over$Ensembl
+#hisat2_total_over_PCA_DEseq2 <- qunDEGAnalysisThreeCondition(round(hisat2_total_over_PCA), "Mock", 3, "NT", 3, "T", 3, 2, 0.01)
 #plotDispEsts(hisat2_total_over1_PCA_DEseq2$DEG, main="Dispersion plot")
 #rld_hisat2_total_over1 <- rlog(hisat2_total_over1_PCA_DEseq2$DEG)
 #plotPCA(rld_hisat2_total_over1)
@@ -133,20 +182,48 @@ hisat2_total_over1_PCA_DEseq2 <- qunDEGAnalysisThreeCondition(round(hisat2_total
 #plotPCA(vst_hisat2_total_over1)
 
 # DEG analysis
-resultsNames(star_total_over1_PCA_DEseq2$DEG)
-resultsNames(hisat2_total_over1_PCA_DEseq2$DEG)
+resultsNames(star_total_over_PCA_DEseq2$DEG)
+
+#resultsNames(hisat2_total_over_PCA_DEseq2$DEG)
 
 # generate results table for NT_vs_Mock
-res_NT_Mock_star <- filterResult(star_total_over1_PCA_DEseq2$DEG, star_total_over1, "condition_NT_vs_Mock", 2, 0.01)
-res_NT_Mock_hisat2 <- filterResult(hisat2_total_over1_PCA_DEseq2$DEG, hisat2_total_over1,"condition_NT_vs_Mock", 2, 0.01)
-res_NT_Mock_star_hisat2 <- res_NT_Mock_star[res_NT_Mock_star$Ensembl %in% res_NT_Mock_hisat2$Ensembl,]
-table(res_NT_Mock_star_hisat2$sig)
-res_NT_Mock_star_hisat2_Over10 <- keepOver1(res_NT_Mock_star_hisat2, 10)
-table(res_NT_Mock_star_hisat2_Over10$sig)
-count(res_NT_Mock_star$Ensembl %in% res_NT_Mock_hisat2$Ensembl)/length(res_NT_Mock_star$Ensembl)
+head(star_total_over_PCA)
+DEG_NT_Mock_star <- qunDEGAnalysisTwoCondition(dat = star_total_over_PCA[,1:6],condition_CT = "Mock", num_CT = 3,
+                                              condition_Treat = "NT", num_Treat = 3, fc = 2, pthreshold = 0.01)
+
+DEG_NT_Mock_star_result <- filterResult(dat = DEG_NT_Mock_star$DEG, nameTag = "", expData = star_total_over, 
+                                        fc = 2, pthreshold = 0.01, tagItem = "two")
+
+DEG_NT_Mock_hisat2 <- qunDEGAnalysisTwoCondition(dat = hisat2_total_over_PCA[,1:6],condition_CT = "Mock", num_CT = 3,
+                                               condition_Treat = "NT", num_Treat = 3, fc = 2, pthreshold = 0.01)
+
+DEG_NT_Mock_hisat2_result <- filterResult(dat = DEG_NT_Mock_hisat2$DEG, nameTag = "", expData = hisat2_total_over, 
+                                        fc = 2, pthreshold = 0.01, tagItem = "two")
+
+#dat=star_total_over_PCA[,4:9]
+#condition_CT="T" 
+#num_CT=3
+#condition_Treat="NT"
+#num_Treat=3
+#fc=2
+#pthreshold=0.01
+
+DEG_NT_T_star <- qunDEGAnalysisTwoCondition(dat = star_total_over_PCA[,4:9],condition_CT = "NT", num_CT = 3,
+                                               condition_Treat = "T", num_Treat = 3, fc = 2, pthreshold = 0.05)
+
+DEG_NT_T_star_result <- filterResult(dat = DEG_NT_T_star$DEG, nameTag = "", expData = star_total_over, 
+                                        fc = 2, pthreshold = 0.01, tagItem = "two")
 
 
-testGene <- res_NT_Mock_star_hisat2_Over10[res_NT_Mock_star_hisat2_Over10$sig == "up",]$Ensembl
+DEG_NT_T_hisat2 <- qunDEGAnalysisTwoCondition(dat = hisat2_total_over_PCA[,4:9],condition_CT = "NT", num_CT = 3,
+                                            condition_Treat = "T", num_Treat = 3, fc = 2, pthreshold = 0.05)
+
+DEG_NT_T_hisat2_result <- filterResult(dat = DEG_NT_T_hisat2$DEG, nameTag = "", expData = hisat2_total_over, 
+                                     fc = 2, pthreshold = 0.01, tagItem = "two")
+
+
+
+testGene <- res_NT_Mock_star_hisat2[res_NT_Mock_star_hisat2$sig == "up",]$Ensembl
 
 #gene.df <- bitr(gene, fromType = "ENTREZID", toType = c("ENSEMBL", "SYMBOL"), OrgDb = org.Hs.eg.db)
 
