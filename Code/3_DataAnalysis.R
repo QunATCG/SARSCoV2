e81f375fd282a341030eb85a9c81cd37781bbbdf
LiQun95 <liqun95@163.com>
2023-12-06 07:25:24 +0100

    update

--- Code/3_DataAnalysis.R
+++ Code/3_DataAnalysis.R
@@ -1,7 +1,7 @@
 ###
  # @Descripttion: Data Analysis
  # @Author: Qun Li
- # @Email: qun.li@ki.se
+ # @Email: qun.li@ki.se/liqun95@163.com
  # @Date: 2023-11-23 14:21:57
  # @LastEditTime: 2023-11-23 14:21:58
 ### 
@@ -26,7 +26,12 @@ library(pheatmap)
 # [1] ‘1.0.12’
 ####
 
-# load tpm, matchedID and sampleinfo
+# load tpm/fpkm/expectedCount/readCount, matchedID and sampleinfo
+# tpm/fpkm/expectedCount were from RSEM (https://github.com/deweylab/RSEM)
+# readCount was from featureCounts (https://subread.sourceforge.net/featureCounts.html)
+# readCount or round down expected Count was used to perform DEG analysis (https://pubmed.ncbi.nlm.nih.gov/35256454/)
+
+# 
 expTPM <- read.table("./Data/ExpRNAseq/STAR/Covid19.gene.tpm", header = T,
                     sep = "\t", stringsAsFactors = FALSE)
 expFPKM <- read.table("./Data/ExpRNAseq/STAR/Covid19.gene.fpkm", header = T,
@@ -40,8 +45,7 @@ matchedID <- read.table("./Data/matchedID.txt", header = T, sep = "\t",
 sampleInfo <- read.table("./Data/Sample_information.txt", header = T,
                         sep = "\t", stringsAsFactors = FALSE)
 
-# round down expected Count was used to perform DEG analysis
-# https://pubmed.ncbi.nlm.nih.gov/35256454/
+
 
 # process NA items of matchedID
 for (i in 1:nrow(matchedID)){
@@ -92,9 +96,9 @@ qunDEGAnalysisThreeCondition <- function(dat, condition_CT, num_CT, condition_Tr
   resultList <- list(DEG =  dds1, coldata = coldata )
   return(resultList)
 }
+
 # expression filter
 # exculde genes with fpkm <= 1 
-
 expDataOver1 <- expData[which(rowMeans(expData[,c("Mock_1_FPKM", "Mock_2_FPKM", "Mock_3_FPKM")]) >=1 | 
                       rowMeans(expData[,c("T_1_FPKM", "T_2_FPKM", "T_3_FPKM")]) >=1 | 
                       rowMeans(expData[,c("NT_1_FPKM", "NT_2_FPKM", "NT_3_FPKM")]) >=1),]
@@ -102,31 +106,41 @@ expDataOver1 <- expData[which(rowMeans(expData[,c("Mock_1_FPKM", "Mock_2_FPKM", 
 # PCA
 expData_PCA <- expDataOver1[,30:38]
 rownames(expData_PCA) <- expDataOver1$Ensembl
-expData_correlation <- cor(log2(expData_PCA + 1))
-pheatmap(expData_correlation)
-
-# check expDataOver1 colnames
-colnames(expDataOver1)
-
-# Mock vs NT
-
-
-kk <- qunDEGAnalysisTwoCondition(round(expData_PCA[,1:6]), "Mock", 3, "NT", 3, 2, 0.01)
 mm <- qunDEGAnalysisThreeCondition(round(expData_PCA), "Mock", 3, "NT", 3, "T", 3, 2, 0.01)
-
+plotDispEsts(mm$DEG, main="Dispersion plot")
+# get normalized readcounts from DESeq2 results
 rld <- rlog(mm$DEG)
-plotPCA(rld)
+plotPCA(rld, ntop = 3000)
 
 rld_mat <- assay(rld)
 rld_cor <- cor(rld_mat) 
 pheatmap(rld_cor)
 
-dds <- estimateSizeFactors(mm$DEG)
-# shifted log of normalized counts
-se <- SummarizedExperiment(log2(counts(dds, normalized=TRUE) + 1),
-                           colData=colData(dds))
-plotPCA( DESeqTransform(se))
-# PCA Analysis
+
+# 
+resultsNames(mm$DEG)
+
+# generate results table for NT_vs_Mock
+filterResult <- function(dat, nameTag, fc, pthreshold){
+  res1 <- data.frame(results(dat, name=nameTag), stringsAsFactors = FALSE, check.names = FALSE)
+  res1$Ensembl <- rownames(res1)
+  res1[which(res1$log2FoldChange >= log2(fc) & res1$padj < pthreshold),'sig'] <- 'up'
+  res1[which(res1$log2FoldChange <= -log2(fc) & res1$padj < pthreshold),'sig'] <- 'down'
+  res1[which(abs(res1$log2FoldChange) <= log2(fc) | res1$padj >= pthreshold),'sig'] <- 'none'
+  res1_select <- subset(res1, sig %in% c('up', 'down'))
+  res <- merge(res1_select, expData, by = "Ensembl", all.x = T)
+  return(res)
+}
 
 
-# DEG for infect and uninfect cells
+
+
+res_NT_Mock <- filterResult(mm$DEG, "condition_NT_vs_Mock", 2, 0.01)
+
+res_T_Mock <- filterResult(mm$DEG, "condition_T_vs_Mock", 2, 0.01)
+
+
+
+
+# Mock vs NT
+
