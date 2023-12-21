3a992b33ed5148effa142cda8e466b354f7acfb0
LiQun95 <liqun95@163.com>
2023-12-21 19:30:18 +0100

    update

--- Code/1_RNASeqPipeline.sh
+++ Code/1_RNASeqPipeline.sh
@@ -3,7 +3,7 @@
  # @Author: Qun Li
  # @Email: qun.li@ki.se
  # @Date: 2023-11-23 11:25:54
- # @LastEditTime: 2023-12-19 13:31:51
+ # @LastEditTime: 2023-12-21 19:29:58
 ### 
 
 #--------------------------------------------------------------------------------
@@ -22,29 +22,38 @@
 #--------------------------------------------------------------------------------
 ### Default setting
 #### samples dir path
-SAMPLE_DIR=
+SAMPLE_DIR=path_samples
 #### genome index path
-GENOMEINDEX=
-#### RSEM index path
-RSEMINDEX=
+GENOMEINDEX=path_genome
 #### gtf path
-GTF=
+GTF=path_GTF
 #### FASTQ1 path
-FQ1=
+FQ1=path_fastq1
 #### FASTQ2 path
-FQ2=
+FQ2=path_fastq2
 #### Threads number
-THREADS=
+THREADS=16
+#### TRIMOUT path
+TRIM_DIR=path_trimOut
 #--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+### 0. Remove rRNA
+#### Hisat2
+#### 0.1 download human rRNA sequence from NCBI
+#### 0.2 build rRNA sequence index (Human_rRNA_Index) using hisat2
+#### 0.3 exclude rRNA sequence
+hisat2 -p $THREADS -x $Human_rRNA_Index -1 $FQ1 -2 $FQ2 --un-conc-gz ${FQ1}_${FQ2}.rRNA.dep.fastq.gz
+#--------------------------------------------------------------------------------
+
 
 #--------------------------------------------------------------------------------
 ### 1. QC and Trim
 #### software: trim_galore
 #### https://www.bioinformatics.babraham.ac.uk/projects/trim_galore/
-
 trim_galore -q 30 -stringency 3 -length 20 --phred33 -fastqc \
             -clip_R1 3 -clip_R2 3 -e 0.1 -dont_gzip \
-            -paired $SAMPLE_DIR/$FQ1 $SAMPLE_DIR/$FQ2 \
+            -paired ${FQ1}_${FQ2}.rRNA.dep.fastq.1.gz ${FQ1}_${FQ2}.rRNA.dep.fastq.2.gz \
             --output_dir $TRIM_DIR
 #--------------------------------------------------------------------------------
 
@@ -53,12 +62,12 @@ trim_galore -q 30 -stringency 3 -length 20 --phred33 -fastqc \
 #### software: Hisat2 
 #### https://daehwankimlab.github.io/hisat2/manual/
 
-hisat2 -p $THREADS -x $GENOMEINDEX -1 $FQ1 -2 $FQ2 \
-    --summary-file summary.txt -S FQ.sam
+hisat2 -p $THREADS -x $GENOMEINDEX -1 ${FQ1}_${FQ2}.rRNA.dep.fastq.1.gz_val_1.fq.gz -2 ${FQ1}_${FQ2}.rRNA.dep.fastq.2.gz_val_2.fq.gz \
+    --summary-file summary.txt -S ${FQ1}_${FQ2}.sam
 
-samtools view -@ $THREADS -bS FQ.sam > FQ.bam;
-samtools sort -@ $THREADS FQ.bam -o FQ.sorted.bam;
-samtools index -@ $THREADS FQ.sorted.bam
+samtools view -@ $THREADS -bS ${FQ1}_${FQ2}.sam > ${FQ1}_${FQ2}.bam;
+samtools sort -@ $THREADS ${FQ1}_${FQ2}.bam -o ${FQ1}_${FQ2}.sorted.bam;
+samtools index -@ $THREADS ${FQ1}_${FQ2}.sorted.bam
 #--------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------
@@ -67,7 +76,7 @@ samtools index -@ $THREADS FQ.sorted.bam
 #### https://subread.sourceforge.net/featureCounts.html
 #### https://htseq.readthedocs.io/en/release_0.11.1/count.html
 #### https://ccb.jhu.edu/software/stringtie/
-featureCounts -T $THREADS -a $GTF -o counts.txt --countReadPairs -p -t exon -g gene_id *.sorted.bam
-htseq-count -f bam -s no -t exon -i gene_id --nonunique=none *.sorted.bam $GTF > htseq.count
-stringtie -e -B -p $THREADS -G $GTF -A FQ_gene_abund.tab -o FQ.gtf FQ.sorted.bam
+featureCounts -T $THREADS -a $GTF -o ${FQ1}_${FQ2}.counts.txt --countReadPairs -p -t exon -g gene_id ${FQ1}_${FQ2}.sorted.bam
+htseq-count -f bam -s no -t exon -i gene_id --nonunique=none ${FQ1}_${FQ2}.sorted.bam $GTF > htseq.count
+stringtie -e -B -p $THREADS -G $GTF -A ${FQ1}_${FQ2}_gene_abund.tab -o ${FQ1}_${FQ2}.gtf ${FQ1}_${FQ2}.sorted.bam
 #--------------------------------------------------------------------------------
